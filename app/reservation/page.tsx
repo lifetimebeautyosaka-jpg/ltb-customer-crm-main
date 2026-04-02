54ed24d12e4fc27cdec14b01653b2d063cb85d2b
lifetimebeautyosaka-jpg <lifetimebeauty.osaka@gmail.com>
2026-04-03 01:09:21 +0900

    復旧

--- app/reservation/page.tsx
+++ app/reservation/page.tsx
@@ -1,25 +1,3 @@
-"use client";
-
-import Link from "next/link";
-
-export default function ReservationPage() {
-  return (
-    <main style={{ padding: "40px", textAlign: "center" }}>
-      <h1>予約ページ（復旧）</h1>
-
-      <a
-        href="/reservation/new"
-        style={{
-          display: "inline-block",
-          marginTop: "20px",
-          padding: "10px 20px",
-          background: "black",
-          color: "white",
-          borderRadius: "10px",
-        }}
-      >
-        新規予約
-      </a>
-    </main>
-  );
+export default function Page() {
+  return <div>OK</div>;
 }
