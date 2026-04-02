80808d421d3d9e952a30a8b7b44e716ffa1694a9
lifetimebeautyosaka-jpg <lifetimebeauty.osaka@gmail.com>
2026-04-02 15:12:34 +0900

    テスト
    テスト

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  "https://eojzjazaaljgawbrxlsy.supabase.co",
+  "sb_publishable_CNfgVNSAESxTsANTFu0jdw_Ix0UdWGA"
 );
